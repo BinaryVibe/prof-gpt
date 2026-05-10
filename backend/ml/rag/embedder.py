b3cbc0c7e71cae3d697928dcbf8f4f66cfe40c25
Ayaan Ahmed <aayan.shazim@gmail.com>
2026-05-10 20:43:57 +0500

    added source in collection

--- backend/ml/rag/embedder.py
+++ backend/ml/rag/embedder.py
@@ -2,6 +2,13 @@ from chromadb.utils import embedding_functions
 
 miniLM_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
-def ingest_chunks(chunks, collection):
-    ids = [f"chunk_{i}" for i in range(len(chunks))]
-    collection.add(ids=ids, documents=chunks)
+def ingest_chunks(chunks, collection, source_name="unknown.pdf"):
+    ids = [f"{source_name}_chunk_{i}" for i in range(len(chunks))]
+    
+    metas = [{"source": source_name} for _ in range(len(chunks))]
+    
+    collection.add(
+        ids=ids, 
+        documents=chunks,
+        metadatas=metas
+    )
